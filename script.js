308fdf3a8204a389885aa69342b0d8bbabdccf27
AurelMarie <88661266+AurelMarie@users.noreply.github.com>
2023-12-22 21:14:29 +0100

    Skill section

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+
+  jQuery(document).ready(function(){
+		jQuery('.skillbar').each(function(){
+			jQuery(this).find('.skillbar-bar').animate({
+				width:jQuery(this).attr('data-percent')
+			},3000);
+		});
+	});
